97a9c908dc4f1c4491b9792dd33bd6297f49f1df
Jay <jaythakkar618@gmail.com>
2023-06-13 14:49:32 -0400

    prepare callbacks

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,7 +1,9 @@
 from cnnClassifier.constants import *
 from cnnClassifier.utils.common import read_yaml, create_directories
 from cnnClassifier.entity.config_entity import (DataIngestionConfig,
-                                                PrepareBaseModelConfig)
+                                                PrepareBaseModelConfig,
+                                                 PrepareCallbackConfig)
+import os
 
 
 class ConfigurationsManager:
@@ -45,3 +47,18 @@ class ConfigurationsManager:
         )
 
         return prepare_base_model_config
+    
+def get_prepare_callback_config(self) -> PrepareCallbackConfig:
+        config = self.config.prepare_callbacks
+        model_ckpt_dir = os.path.dirname(config.checkpoint_model_filepath)
+        create_directories([
+            Path(model_ckpt_dir),
+            Path(config.tensorboard_root_log_dir)
+        ])
+
+        prepare_callback_config = PrepareCallbackConfig(
+            root_dir=Path(config.root_dir),
+            tensorboard_root_log_dir=Path(config.tensorboard_root_log_dir),
+            checkpoint_model_filepath=Path(config.checkpoint_model_filepath)
+            )
+        return prepare_callback_config
